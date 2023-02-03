43ddbb7edc5803d3689e40235ab9e5d1a27710d1
spiritman90 <phemmyadey@gmail.com>
2023-02-03 16:44:29 +0100

    removed warning

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.get("/", (req, res) => {
 app.use(errorHandler);
 // Connect to DB and start server
 const PORT = process.env.PORT || 5000;
+mongoose.set("strictQuery", true);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
